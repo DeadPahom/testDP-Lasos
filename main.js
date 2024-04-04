e32733a05cbb887f06a25daf052a763ac8b27861
SilentBob <wersdf2012zxc@gmail.com>
2024-04-04 14:40:10 +0300

    Ебать рандом

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 let randomNumber = Math.random();
+let currentNumber;
+
+let input = document.querySelector("pole")
+
+
+if (input < randomNumber || input > randomNumber)
+{
+    console .log("Error");
+}
+else
+{
+    console.log("Yes")
+}
+
